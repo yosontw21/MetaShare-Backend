7f65387ade0faddc01261c8241ccb15b1be0247e
yosontw21 <66150719+yosontw21@users.noreply.github.com>
2022-05-15 20:01:08 +0800

    update postman

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,7 +4,6 @@ const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
 userRouter.route('/signup').post(authController.signup);
-
 userRouter.route('/login').post(authController.login);
 
 userRouter
