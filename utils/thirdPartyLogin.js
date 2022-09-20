7f72e24d3d3403188b7825a7323301b128f7c993
yosontw21 <66150719+yosontw21@users.noreply.github.com>
2022-09-21 01:36:52 +0800

    完成google第三方登入

--- utils/thirdPartyLogin.js
+++ utils/thirdPartyLogin.js
@@ -62,7 +62,6 @@ router.get(
 		});
 		const cookieOptions = {
 			expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 86400000),
-			domain: `${process.env.SERVER_BASE_URL}`
 		};
 		res.cookie('jwt', token, cookieOptions);
 		res.redirect(
