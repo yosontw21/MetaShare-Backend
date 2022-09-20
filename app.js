39751674f917ffccf30bd04775b6857dcfd42977
yosontw21 <66150719+yosontw21@users.noreply.github.com>
2022-09-21 00:41:30 +0800

    完成google第三方登入

--- app.js
+++ app.js
@@ -29,14 +29,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors());
 
-app.use(
-	cors({
-		origin: `${process.env.CLIENT_BASE_URL}`,
-		credentials: true
-	})
-);
+// app.use(
+// 	cors({
+// 		origin: `${process.env.CLIENT_BASE_URL}`,
+// 		credentials: true
+// 	})
+// );
 
 app.use((req, res, next) => {
 	req.requestTime = new Date().toLocaleString();
