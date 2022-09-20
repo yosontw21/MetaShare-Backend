803137836b207ec772a7a39f7dad361d4222d252
yosontw21 <66150719+yosontw21@users.noreply.github.com>
2022-09-21 00:31:12 +0800

    完成google第三方登入

--- utils/thirdPartyLogin.js
+++ utils/thirdPartyLogin.js
@@ -18,7 +18,7 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: 'http://localhost:3000/api/auth/google/callback'
+			callbackURL: `${process.env.SERVER_BASE_URL}/api/auth/google/callback`
 		},
 		async (accessToken, refreshToken, profile, cb) => {
 			// console.log(profile);
