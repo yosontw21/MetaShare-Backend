30c5e84cd02a63bec3c6d47c8ac5586d173bbdee
yosontw21 <66150719+yosontw21@users.noreply.github.com>
2022-09-21 02:44:55 +0800

    完成google第三方登入

--- utils/thirdPartyLogin.js
+++ utils/thirdPartyLogin.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
@@ -9,9 +10,18 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/userModel');
 
 const dotenv = require('dotenv');
-const { default: axios } = require('axios');
 dotenv.config({
 	path: './config.env'
+});
+
+passport.serializeUser((user, done) => done(null, user._id));
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+		.then((user) => {
+			console.log('deserializeUser', user);
+			done(null, user);
+		})
+		.catch((err) => done(err, null));
 });
 
 passport.use(
@@ -46,33 +56,36 @@ passport.use(
 		}
 	)
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
-router.get(
-	'/google',
+router.get('/google', (req, res, next) => {
+	if (req.query.redirect) req.session.authRedirect = req.query.redirect;
 	passport.authenticate('google', {
 		scope: ['email', 'profile']
-	})
-);
+	})(req, res, next);
+});
 
-router.get(
-	'/google/callback',
-	passport.authenticate('google', { session: false }),
-	(req, res) => {
-		const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, {
+router.get('/google/callback', passport.authenticate('google'), (req, res) => {
+	const token = jwt.sign(
+		{ id: req.session.passport.user },
+		process.env.JWT_SECRET,
+		{
 			expiresIn: process.env.JWT_EXPIRES_DAY
-		});
-		const cookieOptions = {
-			expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 86400000)
-		};
-		res.cookie('jwt', token, cookieOptions, {
-			domain: `${process.env.CLIENT_BASE_URL}`
-		});
-		res.header();
-		res.redirect(
-			302,
-			`${process.env.CLIENT_BASE_URL}/MetaShare/#/post?token=${token}`
-		);
-	}
-);
+		}
+	);
+	req.session.jwt = token;
+	console.log(	req.session)
+	const cookieOptions = {
+		expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 86400000)
+	};
+	res.cookie('jwt', token, cookieOptions, {
+		domain: `${process.env.CLIENT_BASE_URL}`
+	});
+	res.redirect(
+		302,
+		`${process.env.CLIENT_BASE_URL}/MetaShare/#/post?token=${token}`
+	);
+});
 
 module.exports = router;
